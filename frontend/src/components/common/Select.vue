a985de062c9860dae83cc865c3c3836f59a95333
chihao.ou <ou727963665@gmail.com>
2026-09-13 17:16:26 +0800

    fix(ui): focus non-searchable select dropdowns

--- frontend/src/components/common/Select.vue
+++ frontend/src/components/common/Select.vue
@@ -55,6 +55,7 @@
           :class="[instanceId]"
           :style="dropdownStyle"
           role="listbox"
+          tabindex="-1"
           @click.stop
           @mousedown.stop
           @keydown="onDropdownKeyDown"
@@ -384,6 +385,8 @@ watch(isOpen, (open) => {
 
     if (isSearchable.value) {
       nextTick(() => searchInputRef.value?.focus())
+    } else {
+      nextTick(() => dropdownRef.value?.focus())
     }
     // Add scroll listener to update position
     window.addEventListener('scroll', updateTriggerRect, { capture: true, passive: true })
